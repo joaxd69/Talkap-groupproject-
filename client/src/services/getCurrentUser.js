94ba0c25d5b2264de287757ee58f232b8037b133
unknown <joakig6@gmail.com>
2023-02-27 20:45:10 -0300

    new link

--- client/src/services/getCurrentUser.js
+++ client/src/services/getCurrentUser.js
@@ -2,7 +2,7 @@ import { getCurrentUser } from "../store/slices/users/index.js";
 
 export function getUser(dispatch, email) {
   // fetch(`http://localhost:3001/users/${email}`)
-  fetch(`https://serverdeploy-production.up.railway.app/users/${email}`)
+  fetch(`https://talkap-production-95b1.up.railway.app/${email}`)
     .then((response) => response.json())
     .then((response) => dispatch(getCurrentUser(response)))
 
